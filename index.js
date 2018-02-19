9995258b2f3a05267bb4fffebcdd40da81a7cffb
Dan Kreiger <dan@dankreiger.com>
2018-02-19 23:47:21 +0100

    Add events

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
 var element = {
+  currentSelectVal: null,
+  container: null,
   bootstrap4: function() {
-    if (!document.getElementById('bs4')) {
-      var link = document.createElement('link');
-      link.id = 'bs4';
-      link.rel = 'stylesheet';
-      link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css';
-      document.head.appendChild(link);
-    }
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css';
+    document.head.appendChild(link);
   },
   create: function(elementType, content) {
     var node = document.createElement(elementType),
       elNode = document.createTextNode(content);
+
     node.appendChild(elNode);
     this.render(node);
   },
   remove: function(elementType) {
     var node = document.querySelector(elementType);
     if (node) {
-      document.body.removeChild(node);
+      this.container.removeChild(node);
     };
   },
   render: function(node) {
-    document.body.appendChild(node);
+    if(this.container){
+      this.container.appendChild(node);
+    } else {
+      document.body.appendChild(node);
+    }
   },
-  h1: function(content) {
-    this.remove("H1");
-    this.create("H1", content);
-  },
-  h2: function(content) {
-    this.remove("H2");
-    this.create("H2", content);
-  },
-  handleInputChange: function(e) {
-    var selectNode = document.getElementById('elementsList'),
+  handleInput: function(e) {
+    var selectNode = document.querySelector('select'),
         currentOption = selectNode.options[selectNode.selectedIndex].value;
 
     var text = e.target.value;
-    element[currentOption](text);
+    element.remove(currentOption);
+    element.create(currentOption, text);
+  },
+  handleSelectFocus: function(e) {
+    this.currentSelectVal = e.target.value;
+  },
+  handleSelectChange: function(e) {
+    var selectedEl = e.target.value,
+        oldEl = document.querySelector(this.currentSelectVal);
+    element.remove(this.currentSelectVal);
+    element.create(selectedEl, oldEl ? oldEl.innerText : '');
   },
   init: function() {
-    var input = document.createElement("INPUT"),
-        select = document.createElement("SELECT")
-        options = ["h1","h2"];
-
-    select.id = 'elementsList';
+    var container = document.createElement("DIV"),
+        input = document.createElement("INPUT"),
+        select = document.createElement("SELECT"),
+        css = 'select { display: block; margin-bottom: 2rem;} .container > * + * + * {word-wrap: break-word; width: 100%;}',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style'),
+        options = ["h1","h2", "h3", "h4", "h5", "h6", "p", "code"];
 
     this.bootstrap4();
+
+    container.className += "container";
+    input.className += "form-control";
+    select.className += "form-control";
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
 
     for (var i = 0; i < options.length; i++) {
         var option = document.createElement("option");
@@ -54,9 +75,15 @@ var element = {
         select.appendChild(option);
     }
 
-    input.className += "form-control";
-    select.className += "form-control";
-    input.addEventListener('input', this.handleInputChange, false);
+    input.addEventListener('input', this.handleInput, false);
+    select.addEventListener('focus', this.handleSelectFocus, false);
+    select.addEventListener('click', this.handleSelectFocus, false);
+    select.addEventListener('change', this.handleSelectChange, false);
+
+
+    this.render(container);
+    this.container = document.querySelector('.container');
+
     this.render(input);
     this.render(select);
   }
